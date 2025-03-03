c465c5ff01c9df2568c414af117253f55cec1863
WesleyRivera2007 <wesleybladimir007@gmail.com>
2025-03-02 19:25:40 -0600

    Programa imprime números naturales 1 al 10.

---
+++ 10_Numeros_Naturales.py
@@ -0,0 +1,8 @@
+#Mostrar los primeros 10 números naturales
+#Escribe un programa que imprima los números del 1 al 10.
+
+def calcular():
+    for i in range(1, 11):
+        print(i)
+
+calcular()        
