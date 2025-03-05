bad72537af283258eeb608630dc57ae4c9db6e8e
WesleyRivera2007 <wesleybladimir007@gmail.com>
2025-03-05 12:01:28 -0600

    SumarPrimeros10Numeros

---
+++ Sumar_los_primeros _10_números.py
@@ -0,0 +1,9 @@
+#Sumar los primeros 10 números
+#Usa un bucle para sumar los números del 1 al 10 e imprimir el resultado.
+
+def bucleSumar():
+    suma = 0
+    for i in range(1, 11):
+        suma += i
+    print(f"La suma de primeros 10 numeros es: {suma}")
+bucleSumar()
