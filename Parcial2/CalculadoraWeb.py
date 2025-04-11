d450c973ba620fd84a2cbc0b61479523e5e2f9e2
WesleyRivera2007 <wesleybladimir007@gmail.com>
2025-04-10 22:39:53 -0600

    Calculadora web flask

--- Parcial2/CalculadoraWeb.py
+++ Parcial2/CalculadoraWeb.py
@@ -1,52 +1,38 @@
-from flask  import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///ejemplo.db'
-app.config['SQLALCHEMY_TRACK_AMODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-class Articulo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-
-
-@app.route('/crear-articulo', methods=['GET','POST'])
-def crear_articulo():
-    
-    # PROCESAR EL FORMULARIO
+@app.route('/calculadora-web', methods=['GET', 'POST'])
+def calculadora_flask():
+    resultado = ""
     if request.method == 'POST':
-        num1 = float(request.form('num1'))
-        num2 = float(request.form('num2'))
-        operacion = request.form('operacion')
+        try:
+            num1 = float(request.form["num1"])
+            num2 = float(request.form["num2"])
+            operacion = request.form["operacion"]
 
-        if operacion == 'suma':
-            resultado = num1 + num2
-        
-        
-     
-#get que recibe informacion
-#post q envia informacion
-#patch que modifica parcialmente la info
-#put q modifica completamente la info
-#delete q elimina la info
-
-    return '''
-    <h1>Calculador</h1>
-    <form method="POST">
-    <input type="number" name="num1" placeholder="Primer Numero"><br>
-    <input type="number" name="num2" placeholder="Segundo Numero"><br>
-    <select name="operacion">
-    <button type="submit">Crear</button>
-    </form>
-    '''
-
-@app.route('/articulo/<int:articulo_id>')
-def articulo(articulo_id):
-    return f"articulo {articulo_id}"
-
+            # Realizar operación matemática
+            if operacion == 'suma':
+                resultado = num1 + num2
+                if resultado == 0:
+                    resultado = "La suma es cero."
+            elif operacion == 'resta':
+                resultado = num1 - num2
+                if resultado == 0:
+                    resultado = "La resta es cero."
+            elif operacion == 'multiplicacion':
+                resultado = num1 * num2
+                if resultado == 0:
+                    resultado = "La multiplicación es cero."
+            elif operacion == 'division':
+                if num2 != 0:
+                    resultado = num1 / num2
+                else:
+                    resultado = "No se puede dividir entre 0"
+        except ValueError:
+            resultado = "Por favor ingresa numeros validos."
+    
+    return render_template("index.html", resultado=resultado)
 
 if __name__ == "__main__":
     app.run(debug=True)
