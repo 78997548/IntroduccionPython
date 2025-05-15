33a89ae8f82ea54696663983e86a360a19d5e9c1
WesleyRivera2007 <wesleybladimir007@gmail.com>
2025-05-14 19:24:21 -0600

    Avanze proyecto catedra

--- proyecto/app.py
+++ proyecto/app.py
@@ -2,13 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/consecuencias')
+def consecuencias():
+    return render_template('consecuencias.html')
+
+
 @app.route('/')
 def inicio():
     return render_template('inicio.html')
 
 @app.route('/causas')
 def causas():
-    return render_template('causas.html')
+    return render_template('causa.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
